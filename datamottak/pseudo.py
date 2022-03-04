3377c136dfe6c0e2d711d1f1da5d415829a77f8a
tordr <tordr@reistad.no>
2022-03-04 15:48:20 +0100

    Changed pseudo to work with papis-service

--- datamottak/pseudo.py
+++ datamottak/pseudo.py
@@ -5,39 +5,67 @@ Created on Thu Mar 18 14:34:56 2021
 @author: oeh
 """
 
-from papis_service.http_server import SimpleHttpServer
-from papis_service.client import ClientUrl
-
+from papis_service.common.configureServerEnv import ConfigureServerEnv
 
 class Pseudo:
+  _similar = {"AUTH" : "NONE", "AUTH_LOCATION" : None,
+        "OPERATION" : "BOTH", "KEY_SOURCE" : "KEY_LOCATION",
+        "KEY_TYPE" : "BYTES", "CACHE_LOCATION" : None
+#        ,"CACHE_SOURCE" : "NONE", "CACHE_PARAM" : None
+    }
 
-  def __init__(self, mode = 'test/'):
-    self.mode = mode
+  _test3 = {'CACHE_SOURCE' : "SQL" , "CACHE_PARAM" : 100000,
+             "PSEUDO_ALGORITHM" : "FFX", "KEY_LOCATION" : 
+             'EF4359D8D580AA4F7F036D6F04FC6A94',
+             "KEY_TYPE" : "FROMHEX", "CACHE_LOCATION" : None}
+
+  _cacheTypes = { 
+                   "test3": dict(tuple(_similar.items())+tuple(_test3.items()))
+    }
+  
+  _configDict = { "BROWSER" : _cacheTypes.copy(),
+           "API": _cacheTypes.copy(),
+           "SSL" : False, "ALLOW_GET" : True,
+           "DATABASE" : ":memory:",
+           "HOST" : "localhost", "PORT" : 0}
+  
+  def __init__(self, config = _configDict, interface= 'api', environ = 'test3'):
+    config = ConfigureServerEnv.configure(config)
+    if interface == 'api':
+        env = config.apiEnv.get(environ)
+    elif interface == 'browser':
+        env = config.browserEnv.get(environ)
+    else:
+        raise AttributeError('inteface not api or config')
+        
+    if not env:
+        raise AttributeError(f'Pseudo mode: {environ} not supported. Supported {list(config.apiEnv.keys())}')
+    else:
+        self.cache = env.cache
     
   def close(self):
-      SimpleHttpServer.tearDownClass()
+      pass
     
-  def performPseudo(self, df, varlist):
-    if not getattr(SimpleHttpServer, "TCPServer", None):
-        SimpleHttpServer.setUpClass()
-    pseudo = ClientUrl(SimpleHttpServer.address, self.mode)
-    if not isinstance(varlist, list):
-        varlist = [varlist]
+  def performPseudo(self, df, varlist, encrypt = True):
+    col_set = set()
     for var in varlist:
-      col_list = df[var].tolist()
-      p_dict = pseudo.jsonencryptD(col_list)
-      df[var].replace(p_dict, inplace=True)
+        col_set.update(df[var].tolist())
+    if encrypt:
+        p_dict = self.cache.encryptSet(col_set)
+    else:
+        p_dict = self.cache.decryptSet(col_set)
+    for var in varlist:
+        df[var].replace(p_dict, inplace=True)
 
-  def performPseudoSingleMessage(self, df, varlist):  
-    if not getattr(SimpleHttpServer, "TCPServer", None):
-        SimpleHttpServer.setUpClass()
-    pseudo = ClientUrl(SimpleHttpServer.address, self.mode)
-    if not isinstance(varlist, list):
-        varlist = [varlist]
-    for var in varlist:
-      df[var] = df.apply(
-            lambda x: pseudo.encrypt(x[var]), axis=1)
+#  def performPseudoSingleMessage(self, df, varlist):  
+#    if not getattr(SimpleHttpServer, "TCPServer", None):
+#        SimpleHttpServer.setUpClass()
+#    pseudo = ClientUrl(SimpleHttpServer.address, self.mode)
+#    if not isinstance(varlist, list):
+#        varlist = [varlist]
+#    for var in varlist:
+#      df[var] = df.apply(
+#           lambda x: pseudo.encrypt(x[var]), axis=1)
       
-  def performSasPseudo(self, filename, varlist):
-     raise NotImplementedError()
+
 
